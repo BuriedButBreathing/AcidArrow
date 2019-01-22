a5b8c2e1d01ddbb02538ebd0ed28c928783d0c59
BuriedButBreathing <35439550+BuriedButBreathing@users.noreply.github.com>
2019-01-21 19:27:32 -0500

    Fixed printing of Values
    Fixed how values print, fixed some dumb error handling,

--- AcidArrow.py
+++ AcidArrow.py
@@ -1,30 +1,35 @@
-## AcidArrow V0.1
-## Written by Chris Higgins
-## Requires PyPillow to function
-from PIL import Image as im
-import numpy
-from IO import BytesIO
+# AcidArrow V0.1
+# Written by Chris Higgins
+# Requires PyPillow to function
+from PIL import Image as Im
 
+'''
+NOTES
 
+https://imgur.com/a/DFKFCel URL for PNG file
+https://imgur.com/a/b6UvuqO URL for the BMP file
+using BMP eliminates the need to remove alpha channel values
 
+'''
 
-## TODO:-Strip 4th Value (Alpha Channel) from every array (Use For Loop?) -For loop to standaradize arrays as individual variables Append [char] to variables to prepare for concationation
-
-
-## This section imports an image from the directory the script lives in
-## gathers the values of the individual pixels, and prints out the filetype,
-## dimensions, encoding, and then the pixel values as individual arrays.
+'''
+This section imports an image from the directory the script lives in,
+gathers the values of the individual pixels, and prints out the file type,
+dimensions, encoding, and then the pixel values as individual arrays.
+'''
 
 try:
-    codex = im.open("https://imgur.com/a/DFKFCel")
-    pixelvalues =list(codex.getdata())
-    print("The image is: ")
+    codex = Im.open('test.bmp')
+    pixelvalues = list(codex.getdata())
+    print('The image is: ')
     print(codex.format, codex.size, codex.mode)
-    pixelarray = (pixelvalues)
-    print(pixelvalues)
-    
+    pixelarray = pixelvalues
+    del pixelvalues[18]
+    print(sep="\n", *pixelvalues)
 
-## This section is for error handling
+
+# This section is for error handling
 
 except:
-    print ("Something bad happened and we crashed")
+    print("Something bad happened and we crashed")
+
